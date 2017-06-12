85e189546a4d99ece3e802c83d0c0e7f915674f3
Sam Snyder <ssnyder@tableau.com>
2017-06-12 16:46:00 -0700

    Set default conflict resolution for INSERTs to UPDATE

--- src/main/java/com/gradle/exportapi/dbutil/SQLHelper.java
+++ src/main/java/com/gradle/exportapi/dbutil/SQLHelper.java
@@ -5,15 +5,39 @@ import java.util.stream.Collectors;
 
 public class SQLHelper {
 
-    public static String insert(String body, Object[] params) {
-        return "INSERT INTO " + body + valuesForInsert(params);
+    /**
+     * Produces an INSERT statement that adds values to the table
+     *
+     * Default conflict resolution is to skip the insert
+     *
+     * @param table
+     * @param values
+     * @return
+     */
+    public static String insert(String table, Object[] values) {
+        return insert(table, values, true);
     }
 
+    /**
+     * Produces an INSERT statement that adds values to the table
+     *
+     * @param table name of table to insert values into
+     * @param values values to insert
+     * @param skipOnConflict if false adds 'ON CONFLICT DO NOTHING' to query
+     * @return
+     */
+    public static String insert(String table, Object[] values, boolean skipOnConflict) {
+        String insertStatement = "INSERT INTO " + table + valuesForInsert(values);
+        if(skipOnConflict) {
+            // https://wiki.postgresql.org/wiki/UPSERT
+            insertStatement += " ON CONFLICT DO NOTHING";
+        }
+        return insertStatement;
+    }
 
-
-    private static String valuesForInsert(Object[] params) {
+    private static String valuesForInsert(Object[] values) {
         return " VALUES ("
-                + Arrays.stream(params).map(o -> "?").collect(Collectors.joining(","))
+                + Arrays.stream(values).map(o -> "?").collect(Collectors.joining(","))
                 + ")";
     }
 }
